b2696299f6d4bfec796345310b766a74e3f28f44
Marvinus Kokoh Arif <marvinus.arif@tokopedia.com>
2019-02-06 18:10:52 +0700

    v2.0.4 (#5)
    * fix test on force running when the context almost expires
    
    * reduce lock
    
    * add integration test

--- pipeline_test.go
+++ pipeline_test.go
@@ -3,11 +3,13 @@ package redispipeline_test
 import (
 	"context"
 	"errors"
+	"os"
 	"time"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	. "github.com/redis-pipeline"
+	redisClient "github.com/redis-pipeline/client"
 	mocks "github.com/redis-pipeline/mocks"
 	"github.com/stretchr/testify/mock"
 )
@@ -18,7 +20,7 @@ var (
 	maxConn             int
 )
 
-var _ = Describe("Pipeline", func() {
+var _ = Describe("[Unit Test] Pipeline", func() {
 	var client *mocks.RedisClient
 	BeforeEach(func() {
 		host = "127.0.0.1:30008"
@@ -42,7 +44,7 @@ var _ = Describe("Pipeline", func() {
 	})
 })
 
-var _ = Describe("Pipeline Session", func() {
+var _ = Describe("[Unit Test] Pipeline Session", func() {
 	var client *mocks.RedisClient
 	BeforeEach(func() {
 		host = "127.0.0.1:30008"
@@ -105,7 +107,7 @@ var _ = Describe("Pipeline Session", func() {
 			Expect(resp).Should(Equal(expResp))
 			Expect(err).Should(BeNil())
 		})
-		//
+
 		It("should force to cancel", func() {
 			maxCommandsPerBatch = 1
 			client.On("NewBatch").
@@ -220,6 +222,164 @@ var _ = Describe("Pipeline Session", func() {
 			Expect(resp).Should(BeNil())
 			Expect(err).Should(Equal(expError))
 		})
-
 	})
+})
+
+var _ = Describe("Integration Test Pipeline", func() {
+	if os.Getenv("integration") == "true" {
+		// if true {
+		Context("Redis Single", func() {
+			var (
+				expected []struct {
+					Command string
+					Args    []interface{}
+				}
+				singleClient redisClient.RedisClient
+				pl           RedisPipeline
+			)
+			BeforeEach(func() {
+				host = "127.0.0.1:30008"
+				singleClient = redisClient.New(redisClient.SINGLE_MODE, host, maxConn)
+				pl = NewRedisPipeline(singleClient, maxCommandsPerBatch)
+			})
+
+			It("populate data to redis cluster", func() {
+				expected = []struct {
+					Command string
+					Args    []interface{}
+				}{
+					{
+						Command: "SET",
+						Args:    []interface{}{"foo", "bar"},
+					}, {
+						Command: "SET",
+						Args:    []interface{}{"fox", "lazy"},
+					}, {
+						Command: "SET",
+						Args:    []interface{}{"minny", "manny"},
+					}, {
+						Command: "SET",
+						Args:    []interface{}{"moo", "minny"},
+					},
+				}
+				sess := pl.NewSession(nil)
+				for _, exp := range expected {
+					sess = sess.PushCommand(exp.Command, exp.Args...)
+				}
+				resp, err := sess.Execute()
+				Expect(resp).ShouldNot(BeNil())
+				Expect(err).Should(BeNil())
+			})
+
+			It("get data from redis single node", func() {
+				expected = []struct {
+					Command string
+					Args    []interface{}
+				}{
+					{
+						Command: "GET",
+						Args:    []interface{}{"foo", "bar"},
+					}, {
+						Command: "GET",
+						Args:    []interface{}{"fox", "lazy"},
+					}, {
+						Command: "GET",
+						Args:    []interface{}{"minny", "manny"},
+					}, {
+						Command: "GET",
+						Args:    []interface{}{"moo", "minny"},
+					},
+				}
+				sess := pl.NewSession(nil)
+				for _, exp := range expected {
+					sess = sess.PushCommand(exp.Command, exp.Args[0])
+				}
+				resps, err := sess.Execute()
+				Expect(resps).ShouldNot(BeNil())
+				Expect(err).Should(BeNil())
+
+				for i, resp := range resps {
+					reply := resp.Value.([]byte)
+					Expect(string(reply)).Should(Equal(expected[i].Args[1]))
+				}
+			})
+		})
+
+		Context("Redis Cluster", func() {
+			var (
+				expected []struct {
+					Command string
+					Args    []interface{}
+				}
+				clusterClient redisClient.RedisClient
+				pl            RedisPipeline
+			)
+
+			BeforeEach(func() {
+				host := "127.0.0.1:30001;127.0.0.1:30002;127.0.0.1:30003"
+				clusterClient = redisClient.New(redisClient.CLUSTER_MODE, host, maxConn)
+				pl = NewRedisPipeline(clusterClient, maxCommandsPerBatch)
+			})
+
+			It("populate data to redis cluster", func() {
+				expected = []struct {
+					Command string
+					Args    []interface{}
+				}{
+					{
+						Command: "SET",
+						Args:    []interface{}{"foo", "bar"},
+					}, {
+						Command: "SET",
+						Args:    []interface{}{"fox", "lazy"},
+					}, {
+						Command: "SET",
+						Args:    []interface{}{"minny", "manny"},
+					}, {
+						Command: "SET",
+						Args:    []interface{}{"moo", "minny"},
+					},
+				}
+				sess := pl.NewSession(nil)
+				for _, exp := range expected {
+					sess = sess.PushCommand(exp.Command, exp.Args...)
+				}
+				resp, err := sess.Execute()
+				Expect(resp).ShouldNot(BeNil())
+				Expect(err).Should(BeNil())
+			})
+
+			It("get data from redis cluster", func() {
+				expected = []struct {
+					Command string
+					Args    []interface{}
+				}{
+					{
+						Command: "GET",
+						Args:    []interface{}{"foo", "bar"},
+					}, {
+						Command: "GET",
+						Args:    []interface{}{"fox", "lazy"},
+					}, {
+						Command: "GET",
+						Args:    []interface{}{"minny", "manny"},
+					}, {
+						Command: "GET",
+						Args:    []interface{}{"moo", "minny"},
+					},
+				}
+				sess := pl.NewSession(nil)
+				for _, exp := range expected {
+					sess = sess.PushCommand(exp.Command, exp.Args[0])
+				}
+				resps, err := sess.Execute()
+				Expect(resps).ShouldNot(BeNil())
+				Expect(err).Should(BeNil())
+				for i, resp := range resps {
+					reply := resp.Value.([]byte)
+					Expect(string(reply)).Should(Equal(expected[i].Args[1]))
+				}
+			})
+		})
+	}
 })
